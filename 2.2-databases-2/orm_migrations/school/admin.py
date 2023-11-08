62838ba00945fa48d776700f27631b6eb4831e97
Kushnirchuk Maxim <maximivanovgaz@gmail.com>
2023-11-08 15:01:57 +0300

    orm migration

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+# class StudentTeacherInline(admin.TabularInline):
+#     model = school_student_teachers
+#     extra = 1
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ['id', 'name', 'group']
+    # inlines = ['StudentTeacherInline']
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'subject']
