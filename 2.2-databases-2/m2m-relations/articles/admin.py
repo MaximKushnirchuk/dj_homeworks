8cb55331c52caecb664a5173113ba48f65d3c4ff
Kushnirchuk Maxim <maximivanovgaz@gmail.com>
2023-11-09 13:07:42 +0300

    second task var.1`

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Tag, Scope
 
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 2
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image']    
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,19 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['published_at']
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30,  verbose_name='Тег')
+    articles = models.ManyToManyField(Article, related_name='scopes', through='Scope')
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    
+    title = models.ForeignKey(Article, on_delete=models.CASCADE)
+    tag_name = models.ForeignKey(Tag, on_delete=models.CASCADE)
