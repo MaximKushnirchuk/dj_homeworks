237de1f605728f263cc128bb32c55dff5eb8df70
Kushnirchuk Maxim <maximivanovgaz@gmail.com>
2023-12-06 20:42:28 +0300

    3.3 ready

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from advertisements.models import Advertisement
 
@@ -9,35 +10,66 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name',)
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+        )
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
     """Serializer для объявления."""
 
-    # creator = UserSerializer(
-    #     read_only=True,
-    # )
+    creator = UserSerializer(
+        read_only=True,
+    )
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator', 'status', 'created_at')
+        fields = ("id", "title", "description", "creator", "status", "created_at")
 
-    # def create(self, validated_data):
-    #     """Метод для создания"""
+    def create(self, validated_data):
+        """Метод для создания"""
 
-    #     # Простановка значения поля создатель по-умолчанию.
-    #     # Текущий пользователь является создателем объявления
-    #     # изменить или переопределить его через API нельзя.
-    #     # обратите внимание на `context` – он выставляется автоматически
-    #     # через методы ViewSet.
-    #     # само поле при этом объявляется как `read_only=True`
-    #     validated_data["creator"] = self.context["request"].user
-    #     return super().create(validated_data)
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
 
-    # def validate(self, data):
-    #     """Метод для валидации. Вызывается при создании и обновлении."""
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        print(" - - VALIDATE - - ")
+        count_open_status = 0
+        list_advertisement = Advertisement.objects.all()
+        for adv in list_advertisement:
+            if (
+                adv.creator.id == self.context["request"].user.id
+                and adv.status == "OPEN"
+            ):
+                count_open_status += 1
+        print("Было насчитано ", count_open_status, "объявленийБ со статусом : OPEN")
+        if self.context["request"].method == "POST":
+            if count_open_status == 10:
+                raise ValidationError(
+                    "Нельзя иметь больше 10 объявлений со татусом : : OPEN"
+                )
+            print(
+                "Количество объявдений со статусом OPEN теперь равно",
+                count_open_status + 1,
+            )
 
-    #     # TODO: добавьте требуемую валидацию
+        elif self.context["request"].method == "PATCH":
+            if (
+                count_open_status == 10
+                and self.context["request"].data["status"] == "OPEN"
+            ):
+                raise ValidationError(
+                    "Нельзя иметь больше 10 объявлений со татусом: OPEN"
+                )
 
-    #     return data
+        return data
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,17 +1,26 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
+from advertisements.filters import AdvertisementFilter
+from advertisements.permissions import IsOwnerOrReadOnly
+
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-    
-    queryset = Advertisement.objects.all()
-    serializers_class = AdvertisementSerializer
 
-    # def get_permissions(self):
-    #     """Получение прав для действий."""
-    #     if self.action in ["create", "update", "partial_update"]:
-    #         return [IsAuthenticated()]
-    #     return []
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
